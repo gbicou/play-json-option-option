b438ed40526e1f6af673d34c38765a0cb5dda5c7
bicouy0 <github@bicou.com>
2016-11-16 11:02:56 +0100

    refactor

--- src/test/scala/com/bicou/play/json/JsonOptionOptionSpec.scala
+++ src/test/scala/com/bicou/play/json/JsonOptionOptionSpec.scala
@@ -5,102 +5,80 @@ import org.scalatest._
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
 
-case class A(
-  id: Option[Option[Int]],
-  name: Option[Option[String]]
-)
-
-object A {
-  
-  implicit val attrsReads: Reads[A] = (
-    (__ \ 'id).readOptionOption[Int] and
-    (__ \ 'name).readOptionOption[String]
-  )(A.apply _)
-
-  implicit val attrsWrites: Writes[A] = (
-    (__ \ 'id).writeOptionOption[Int] and
-    (__ \ 'name).writeOptionOption[String]
-  )(unlift(A.unapply))
-
-}
+case class A(id: Option[Option[Int]], name: Option[Option[String]])
 
 class JsonOptionOptionSpec extends WordSpec with Matchers {
-  
+
   "readOptionOption" should {
 
+    implicit val aReads: Reads[A] = (
+      (__ \ 'id).readOptionOption[Int] and
+      (__ \ 'name).readOptionOption[String]
+    )(A.apply _)
+
     "read defined as Some(Some)" in {
-
-      val js = """{"id": 1, "name": "Ben"}"""
-
-      Json.parse(js).validate[A].get should be(A(
-        id = Some(Some(1)),
-        name = Some(Some("Ben"))
-      ))
+      Json.parse("""{"id": 1, "name": "Ben"}""").validate[A].get should be(
+        A(id = Some(Some(1)), name = Some(Some("Ben")))
+      )
     }
 
     "read null as Some(None)" in {
-
-      val js = """{"id": null, "name": null}"""
-
-      Json.parse(js).validate[A].get should be(A(
-        id = Some(None),
-        name = Some(None)
-      ))
+      Json.parse("""{"id": null, "name": null}""").validate[A].get should be(
+        A(id = Some(None), name = Some(None))
+      )
     }
 
     "read missing as None" in {
-
-      val js = """{"name": "Ben"}"""
-
-      Json.parse(js).validate[A].get should be(A(
-        id = None,
-        name = Some(Some("Ben"))
-      ))
+      Json.parse("""{"name": "Ben"}""").validate[A].get should be(
+        A(id = None, name = Some(Some("Ben")))
+      )
     }
-  
+
   }
 
   "writeOptionOption" should {
 
+    implicit val aWrites: Writes[A] = (
+      (__ \ 'id).writeOptionOption[Int] and
+      (__ \ 'name).writeOptionOption[String]
+    )(unlift(A.unapply))
+
     "write Some(Some) as defined" in {
-
-      val a = A(
-        id = Some(Some(1)),
-        name = Some(Some("Ben"))
+      Json.toJson(A(id = Some(Some(1)), name = Some(Some("Ben")))) should be(
+        Json.obj("id" -> 1, "name" -> "Ben")
       )
-
-      Json.toJson(a) should be(Json.obj(
-        "id" -> 1,
-        "name" -> "Ben"
-      ))
     }
 
     "write Some(None) as null" in {
-
-      val a = A(
-        id = Some(None),
-        name = Some(None)
+      Json.toJson(A(id = Some(None), name = Some(None))) should be(
+        Json.obj("id" -> JsNull, "name" -> JsNull)
       )
-
-      Json.toJson(a) should be(Json.obj(
-        "id" -> JsNull,
-        "name" -> JsNull
-      ))
     }
 
     "write None as missing" in {
-
-      val a = A(
-        id = None,
-        name = Some(Some("Ben"))
+      Json.toJson(A(id = None, name = Some(Some("Ben")))) should be(
+        Json.obj("name" -> "Ben")
       )
-
-      Json.toJson(a) should be(Json.obj(
-        "name" -> "Ben"
-      ))
     }
 
   }
 
-}
+  "formatOptionOption" should {
 
+    implicit val aFormat: Format[A] = (
+      (__ \ 'id).formatOptionOption[Int] and
+      (__ \ 'name).formatOptionOption[String]
+    )(A.apply, unlift(A.unapply))
+
+    "read / write" in {
+
+      val a = A(id = Some(None), name = Some(Some("Ben")))
+
+      Json.toJson(a) should be(Json.obj("id" -> JsNull, "name" -> "Ben"))
+
+      val js = """{"id": null, "name": "Ben"}"""
+
+      Json.parse(js).validate[A].get should be(a)
+    }
+  }
+}
